cb700d97258e5d4b02d1e6dda601457614d4babf
Bindushree <141547551+bindupes@users.noreply.github.com>
2024-12-31 11:09:31 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,4 @@
+with open("C:\\Users\\17bin\\OneDrive\\Desktop\\Documents\\PERSONAL_GROWTH\\DATA SCIENCE\\example1.txt",'r') as file1:
+    file_stuff=file1.read()
+    print(file_stuff)
+print(file1.close)
